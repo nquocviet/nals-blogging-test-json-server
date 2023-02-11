a0daf3ad7d2573ca849b9c2ddd07baacf352dfe2
VietNguyen27 <nquocviet2307@gmail.com>
2023-02-11 17:53:56 +0700

    Fix error readonly in json-server

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const jsonServer = require("json-server");
 const cors = require("cors");
+const fs = require("fs");
+const os = require("os");
+
+fs.copyFile("db.json", os.tmpdir() + "/db.json", function (err) {
+  if (err) console.log(err);
+  else console.log("copy file succeed to" + os.tmpdir());
+});
+
 const server = jsonServer.create();
 const middlewares = jsonServer.defaults({
   static: "./build",
   noCors: true,
 });
-const router = jsonServer.router("db.json");
+const router = jsonServer.router(path.resolve(os.tmpdir() + "/db.json"));
 const port = process.env.PORT || 5000;
 
 server.use(cors());
